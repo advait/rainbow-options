1193591a13c062e4bbe551cff34281119b52b237
Advait Shinde <advait.shinde@gmail.com>
2020-03-12 13:08:57 -0700

    Basic shader rendering!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {GPU} from "gpu.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Canvas />
   );
+}
+
+class Canvas extends React.Component {
+    componentDidMount() {
+        const canvas = this.refs.canvas;
+        renderCanvas(canvas);
+    }
+
+    render() {
+        return(
+            <div>
+                <canvas id="main-canvas" ref="canvas" />
+            </div>
+        )
+    }
+}
+
+function renderCanvas(canvas) {
+    const gl = canvas.getContext('webgl2');
+    const gpu = new GPU({canvas, context: gl});
+
+    const width = 500;
+    const color = function (x) {
+        this.color(this.thread.x / 500, this.thread.y / 500, x[0], 1);
+    };
+    const render = gpu
+        .createKernel(color)
+        .setOutput([width, width / 1.6])
+        .setGraphical(true);
+
+    render([0.8]);
 }
 
 export default App;
