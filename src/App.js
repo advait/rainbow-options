c8eb6177587d91cdd8744c1cfefa711b2e4519f5
Advait Shinde <advait.shinde@gmail.com>
2020-03-13 11:56:31 -0700

    Add d3 scale.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import {portfolioCanvas} from "./blackscholes";
 import {portfolio} from "./portfolio";
+import * as d3 from "d3";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ class Canvas extends React.Component {
 
   componentDidMount() {
     this.updateCanvas();
+    this.updateD3();
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
@@ -36,6 +38,7 @@ class Canvas extends React.Component {
   }
 
   randomize() {
+    return;
     this.setState({
       ...this.state,
       yFinal: Math.random() * (1200 - 800) + 800,
@@ -58,8 +61,9 @@ class Canvas extends React.Component {
       console.log("No canvas container");
       return;
     }
-    const width = container.offsetWidth || 100.;
-    const height = container.offsetHeight || 100.;
+    const scaleDownFactor = 1;
+    const width = container.offsetWidth / scaleDownFactor || 100.;
+    const height = container.offsetHeight / scaleDownFactor || 100.;
 
     // const canvas = euroCallCanvas(width, height, 2, 0, 0, 200, 100, 0.01, 0.2);
     const canvas = portfolioCanvas(
@@ -74,6 +78,30 @@ class Canvas extends React.Component {
         this.state.sigma);
 
     container.appendChild(canvas);
+  }
+
+  updateD3() {
+    const container = this.canvasContainerRef.current;
+    if (!container) {
+      console.log("No canvas container");
+      return;
+    }
+    const width = container.offsetWidth || 100.;
+    const height = container.offsetHeight || 100.;
+
+    const svg = d3.create("svg")
+        .attr("viewBox", [0, 0, width, height]);
+
+    const xScale = d3.scaleLinear()
+        .domain([this.state.x0, this.state.xFinal])
+        .range([0, width]);
+
+    const xAxis = d3.axisBottom().scale(xScale);
+
+    debugger;
+    svg.append("g").call(xAxis);
+
+    container.appendChild(svg.node());
   }
 }
 
