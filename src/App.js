68fbc49d433e1ed9e00dc8845b4723bb8b3b8234
Advait Shinde <advait.shinde@gmail.com>
2020-03-13 17:59:26 -0700

    Add basic Icons.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import {portfolioCanvas} from "./blackscholes";
 import {portfolio} from "./portfolio";
 import * as d3 from "d3";
 import {makeStyles} from '@material-ui/core/styles';
-import {AppBar, Button, Drawer, IconButton, Toolbar, Typography} from '@material-ui/core';
+import {AppBar, Button, Drawer, Icon, IconButton, Toolbar, Typography} from '@material-ui/core';
 import 'typeface-roboto';
 import Divider from "@material-ui/core/Divider";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Link from "@material-ui/core/Link";
 
 const drawerWidth = 300;
 
@@ -21,6 +25,9 @@ const useStyles = makeStyles(theme => ({
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
   },
+  titleLink: {
+    color: '#FFF',
+  },
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
@@ -29,6 +36,9 @@ const useStyles = makeStyles(theme => ({
     width: drawerWidth,
   },
   toolbar: theme.mixins.toolbar,
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
   content: {
     top: 0,
     right: 0,
@@ -48,12 +58,26 @@ function App() {
       <div className={classes.root}>
         <AppBar position="fixed" className={classes.appBar}>
           <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-            </IconButton>
-            <Typography variant="h6">
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
+            <Icon edge="start" className={classes.menuButton}>
+              <Link href="/" underline='none' className={classes.titleLink}>
+                <TrendingUpIcon/>
+              </Link>
+            </Icon>
+            <Link href="/" underline='none' variant="h6" className={classes.titleLink}>
+              Options
+            </Link>
+            <div style={{flexGrow: 1}}/>
+            <div>
+              <IconButton
+                  aria-label="link to github"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  color="inherit">
+                <Link href="https://www.github.com/" className={classes.titleLink} target="_blank">
+                  <GitHubIcon/>
+                </Link>
+              </IconButton>
+            </div>
           </Toolbar>
         </AppBar>
         <Drawer
