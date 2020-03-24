f36e0b7831951be523a286567520cbe9c766229a
Advait Shinde <advait.shinde@gmail.com>
2020-03-24 11:57:03 -0700

    Add toggle-able header to each option card.

--- client/src/left-drawer.tsx
+++ client/src/left-drawer.tsx
@@ -1,4 +1,4 @@
-import {Box, Button, ButtonGroup, Card, Drawer, Theme} from "@material-ui/core";
+import {Avatar, Box, Button, ButtonGroup, Card, CardHeader, Collapse, Drawer, Theme} from "@material-ui/core";
 import {deepOrange, deepPurple, grey} from "@material-ui/core/colors";
 import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
@@ -8,8 +8,10 @@ import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import clsx from 'clsx';
 import moment from "moment";
-import React from "react";
+import React, {useState} from "react";
 import {Leg, portfolioEntryCost} from "./portfolio";
 
 export const drawerWidth = 350;
@@ -110,8 +112,20 @@ const optionLegStyles = makeStyles((theme: Theme) => ({
     marginRight: theme.spacing(2),
     marginTop: theme.spacing(2),
     marginBottom: theme.spacing(1),
-    paddingTop: theme.spacing(2),
     overflow: "visible",
+  },
+  cardHeader: {
+    cursor: "pointer",
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
   },
   contentRow: {
     display: "flex",
@@ -179,90 +193,105 @@ type OptionLegCardProps = {
 
 function OptionLegCard(props: OptionLegCardProps) {
   const classes = optionLegStyles();
+  const [expanded, setExpanded] = useState(false);
+  const toggleExpanded = () => setExpanded(!expanded);
 
   return (
       <Card elevation={1} className={classes.card}>
-        <Box flexDirection="row" className={classes.contentRow}>
-          <ButtonGroup orientation="horizontal" variant="outlined" className={classes.largeButtonGroup}>
-            <Button
-                className={classes.largeButton}>{props.leg.quantity && props.leg.quantity < 0 ? "Short" : "Long"} Call</Button>
-            <Button
-                className={classes.largeButton}>{props.leg.quantity && props.leg.quantity < 0 ? "Short" : "Long"} Put</Button>
-          </ButtonGroup>
-          <IconButton edge="end"><DeleteIcon/></IconButton>
-        </Box>
+        <CardHeader
+            avatar={<Avatar>CS</Avatar>}
+            title="Hello World"
+            subheader={"This is a test"}
+            action={<IconButton className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}><ExpandMoreIcon/></IconButton>}
+            onClick={toggleExpanded}
+            className={classes.cardHeader}
+        />
 
-        <Box flexDirection="row" className={classes.contentRow}>
-          <ButtonGroup orientation="vertical" variant="outlined" className={classes.smallButtonGroup}>
-            <Button size="small" className={classes.smallButton}>+</Button>
-            <Button size="small" className={classes.smallButton}>-</Button>
-          </ButtonGroup>
-          <div className={classes.descriptionValueParent}>
+        <Collapse in={expanded} timeout="auto">
+          <Box flexDirection="row" className={classes.contentRow}>
+            <ButtonGroup orientation="horizontal" variant="outlined" className={classes.largeButtonGroup}>
+              <Button
+                  className={classes.largeButton}>{props.leg.quantity && props.leg.quantity < 0 ? "Short" : "Long"} Call</Button>
+              <Button
+                  className={classes.largeButton}>{props.leg.quantity && props.leg.quantity < 0 ? "Short" : "Long"} Put</Button>
+            </ButtonGroup>
+            <IconButton edge="end"><DeleteIcon/></IconButton>
+          </Box>
+
+          <Box flexDirection="row" className={classes.contentRow}>
+            <ButtonGroup orientation="vertical" variant="outlined" className={classes.smallButtonGroup}>
+              <Button size="small" className={classes.smallButton}>+</Button>
+              <Button size="small" className={classes.smallButton}>-</Button>
+            </ButtonGroup>
+            <div className={classes.descriptionValueParent}>
             <span className={classes.description}>
               Expiration
             </span>
-            <span className={classes.value}>
+              <span className={classes.value}>
                 {props.leg.t
                     ?
                     <React.Fragment>{props.leg.t.format("MMM DD, YY")} ({props.leg.t.diff(moment(), "days")} days)</React.Fragment>
                     : "Unknown"
                 }
               </span>
-          </div>
-          <IconButton edge="end"><EditIcon/></IconButton>
-        </Box>
+            </div>
+            <IconButton edge="end"><EditIcon/></IconButton>
+          </Box>
 
-        <Box flexDirection="row" className={classes.contentRow}>
-          <ButtonGroup orientation="vertical" variant="outlined" className={classes.smallButtonGroup}>
-            <Button size="small" className={classes.smallButton}>+</Button>
-            <Button size="small" className={classes.smallButton}>-</Button>
-          </ButtonGroup>
-          <div className={classes.descriptionValueParent} style={{width: "65px"}}>
+          <Box flexDirection="row" className={classes.contentRow}>
+            <ButtonGroup orientation="vertical" variant="outlined" className={classes.smallButtonGroup}>
+              <Button size="small" className={classes.smallButton}>+</Button>
+              <Button size="small" className={classes.smallButton}>-</Button>
+            </ButtonGroup>
+            <div className={classes.descriptionValueParent} style={{width: "65px"}}>
               <span className={classes.description}>
                 Quantity
               </span>
-            <span className={classes.value}>
+              <span className={classes.value}>
               {props.leg.quantity ? props.leg.quantity : "?"}
               </span>
-          </div>
-          <ButtonGroup orientation="vertical" variant="outlined" className={classes.smallButtonGroup}>
-            <Button size="small" className={classes.smallButton}>+</Button>
-            <Button size="small" className={classes.smallButton}>-</Button>
-          </ButtonGroup>
-          <div className={classes.descriptionValueParent}>
+            </div>
+            <ButtonGroup orientation="vertical" variant="outlined" className={classes.smallButtonGroup}>
+              <Button size="small" className={classes.smallButton}>+</Button>
+              <Button size="small" className={classes.smallButton}>-</Button>
+            </ButtonGroup>
+            <div className={classes.descriptionValueParent}>
               <span className={classes.description}>
                 Strike Price
               </span>
-            <span className={classes.value}>
+              <span className={classes.value}>
                 ${props.leg.k ? props.leg.k.toFixed(2) : "?"}
               </span>
-          </div>
-          <IconButton edge="end"><EditIcon/></IconButton>
-        </Box>
+            </div>
+            <IconButton edge="end"><EditIcon/></IconButton>
+          </Box>
 
-        <Box flexDirection="row" className={classes.contentRow}>
-          <ButtonGroup orientation="vertical" variant="outlined" className={classes.smallButtonGroup}>
-            <Button size="small" className={classes.smallButton}>+</Button>
-            <Button size="small" className={classes.smallButton}>-</Button>
-          </ButtonGroup>
-          <div className={classes.descriptionValueParent} style={{width: "60px"}}>
+          <Box flexDirection="row" className={classes.contentRow}>
+            <ButtonGroup orientation="vertical" variant="outlined" className={classes.smallButtonGroup}>
+              <Button size="small" className={classes.smallButton}>+</Button>
+              <Button size="small" className={classes.smallButton}>-</Button>
+            </ButtonGroup>
+            <div className={classes.descriptionValueParent} style={{width: "60px"}}>
               <span className={classes.description}>
                 Unit Price
               </span>
-            <span className={classes.value}>
+              <span className={classes.value}>
                 ${props.leg.k ? props.leg.k.toFixed(2) : "?"}
               </span>
-          </div>
-          <div className={classes.descriptionValueParent}>
+            </div>
+            <div className={classes.descriptionValueParent}>
               <span className={classes.description}>
                 Implied Volatility
               </span>
-            <span className={classes.value}>
+              <span className={classes.value}>
                 ${props.leg.k ? props.leg.k.toFixed(2) : "?"}
               </span>
-          </div>
-          <IconButton edge="end"><EditIcon/></IconButton>
-        </Box>
+            </div>
+            <IconButton edge="end"><EditIcon/></IconButton>
+          </Box>
+        </Collapse>
       </Card>
   )
 }
