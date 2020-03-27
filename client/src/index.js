20c23d8b0c05cda4ffadcc3ab4031496556c90c0
Advait Shinde <advait.shinde@gmail.com>
2020-03-27 11:07:13 -0700

    Add react-router support.

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { wrapReact } from "./graphql";
+import { HashRouter, Switch, Route } from "react-router-dom";
 
-ReactDOM.render(wrapReact(<App />), document.getElementById("root"));
+ReactDOM.render(
+  wrapReact(
+    <HashRouter>
+      <Switch>
+        <Route path="/p/:p">
+          <App />
+        </Route>
+        <Route>
+          <App />
+        </Route>
+      </Switch>
+    </HashRouter>
+  ),
+  document.getElementById("root")
+);
